44f185131200a9aa278818fd9ff0c51b48737092
Arseny Petrovich <strangesound@gmail.com>
2022-11-08 22:21:39 +0300

    debug

--- src/script.js
+++ src/script.js
@@ -1,120 +1,119 @@
 import './style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import gsap from 'gsap'
+import * as dat from 'dat.gui'
 
-import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
-const scene = new THREE.Scene()
-
-const geometry = new THREE.BufferGeometry()
-
-const count = 50
-const positionArray = new Float32Array(count * 3 * 3)
-
-for (let index = 0; index < count * 3 * 3; index++) {
-    positionArray[index] = (Math.random() - .5)*5
-    
+const parameters = {
+    color: 0xff9200,
+    spin: () => {
+        gsap.to(mesh.rotation, {duration:1, y: mesh.rotation.y + 5})
+    }
 }
 
-const positionAttribute = new THREE.BufferAttribute(positionArray, 3)
-geometry.setAttribute('position', positionAttribute)
 
-const material = new THREE.MeshBasicMaterial({
-    color: 0x0000ff,
-    wireframe: true
-})
+/**
+ * 
+ * Base
+ */
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+/**
+ * Object
+ */
+const geometry = new THREE.BoxBufferGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: parameters.color })
 const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
 
-// sizes
+
+// Debug
+const gui = new dat.GUI()
+gui
+    .add(mesh.position, 'y')
+    .min(-3)
+    .max(3)
+    .step(0.01)
+    .name('cubeY')
+gui.add(mesh.position, 'x', -3, 3, 0.01)
+gui.add(mesh.position, 'z', -3, 3, 0.01)
+
+gui
+    .add(mesh, 'visible')
+gui
+    .add(material, 'wireframe')
+gui
+    .addColor(parameters, 'color')
+    .onChange(() => {
+        material.color.set(parameters.color)
+    })
+gui
+    .add(parameters, 'spin')
+
+/**
+ * Sizes
+ */
 const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
 }
 
 window.addEventListener('resize', () => {
+    // Update sizes
     sizes.width = window.innerWidth
     sizes.height = window.innerHeight
 
-    // update camera
+    // Update camera
     camera.aspect = sizes.width / sizes.height
     camera.updateProjectionMatrix()
+
+    // Update renderer
     renderer.setSize(sizes.width, sizes.height)
-
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-
-const cursor = {
-    x: 0,
-    y: 0
-}
-
-window.addEventListener('mousemove', (e) => {
-    cursor.x = -(e.clientX / sizes.width - 0.5)
-    cursor.y = e.clientY / sizes.height - 0.5
-})
-
-window.addEventListener('dblclick', (e) => {
-    const fullscreenElement = document.fullscreenElement || document.webkitFullscreenElement
-    if (!fullscreenElement) {
-
-        if (canvas.requestFullscreen) {
-            canvas.requestFullscreen()
-        }
-        else if (canvas.webkitRequestFullscreen) {
-            canvas.webkitRequestFullscreen()
-        }
-    }
-    else {
-        if (document.exitFullscreen) {
-            document.exitFullscreen()
-        }
-        else if (document.webkitExitFullscreen) {
-            document.webkitExitFullscreen()
-        }
-    }
-    // console.log('document.fullscreenElement');
-    // console.log(document.fullscreenElement);
-})
-
-
-
-mesh.rotation.reorder('YXZ')
-
-scene.add(mesh)
-
-const axesHelper = new THREE.AxesHelper(2)
-scene.add(axesHelper)
-
-
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
 camera.position.z = 3
-scene.add.camera
-
-const canvas = document.querySelector('canvas.webgl')
+scene.add(camera)
 
 // Controls
-
 const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
-const clock = new THREE.Clock()
 
-const tick = () => {
-
-    const elapsedTime = clock.getElapsedTime()
-    // geometry.rotation.x = elapsedTime
-
-    // update controls
-    controls.update()
-    renderer.render(scene, camera)
-    window.requestAnimationFrame(tick)
-}
-
+/**
+ * Renderer
+ */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas,
-    antialias: true
-
+    canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+
+const tick = () => {
+    const elapsedTime = clock.getElapsedTime()
+
+    // Update controls
+    controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
 
 tick()
